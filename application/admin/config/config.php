65546fa79575a44a9d6913369919b35caa1cefbd
teek <xulei-china@hotmail.com>
2016-04-14 19:47:41 +0800

    suitable for my environment 2

--- application/admin/config/config.php
+++ application/admin/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://site1.demo.com/';
+$config['base_url'] = 'http://localhost/project_on/cxpcms/';
 
 /*
 |--------------------------------------------------------------------------
